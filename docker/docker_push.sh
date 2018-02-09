4788a6d7deb3909d468a7ee173bdb544e26b3949
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2018-02-09 15:23:30 -0600

    chore(Docker): move login

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -3,5 +3,4 @@ echo "building brickster/my-box:$TRAVIS_TAG"
 mv build/libs/my-box-1.0-SNAPSHOT.jar docker/app.jar
 cd docker
 docker build -t brickster/my-box -t brickster/my-box:$TRAVIS_TAG .
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker push brickster/my-box
