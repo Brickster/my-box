9471023004d89e4ee576b3f62554fc3a7789e6a8
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2018-02-09 20:12:11 -0600

    chore(Docker): remove push script

--- docker/docker_push.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "building brickster/my-box:$TRAVIS_TAG"
-mv build/libs/my-box-1.0-SNAPSHOT.jar docker/app.jar
-cd docker
-docker build -t brickster/my-box -t brickster/my-box:$TRAVIS_TAG .
-docker push brickster/my-box
